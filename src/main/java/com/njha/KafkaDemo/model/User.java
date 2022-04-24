737f0af8b6b3872c3e202bee952709137718f5ec
Narendra Jha <njha.sde@gmail.com>
2022-04-24 12:36:47 +0530

    cleanup

--- src/main/java/com/njha/KafkaDemo/model/User.java
+++ src/main/java/com/njha/KafkaDemo/model/User.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User implements Serializable {
+public class User {
     private Long id;
     private String name;
     private Integer age;
